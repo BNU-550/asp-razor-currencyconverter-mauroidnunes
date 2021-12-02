cb2db40023249c50908a5d55a8d2fea94011ce7b
Mauro Nunes <21815118@bucks.ac.uk>
2021-12-02 19:50:19 +0000

    finished

--- RazorCurrencyConverter/RazorCurrencyConverter/Pages/Index.cshtml.cs
+++ RazorCurrencyConverter/RazorCurrencyConverter/Pages/Index.cshtml.cs
@@ -10,6 +10,9 @@ namespace RazorCurrencyConverter.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public string FullName { get; set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,7 +22,19 @@ namespace RazorCurrencyConverter.Pages
 
         public void OnGet()
         {
-
+            FullName = "Mauro Nunes";
+        }
+        public void OnPost()
+        {
+            if (String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["Message"] = "You have not entered a name!";
+                FullName = "Anonymous";
+            }
+            else
+            {
+                ViewData["Message"] = "Name registered!";
+            }
         }
     }
 }
